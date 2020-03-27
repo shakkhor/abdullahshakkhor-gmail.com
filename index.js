fd7b6896287ee167257605593f90384fff2be94f
shakkhor <abdullahshakkhor@gmail.com>
2020-03-27 21:29:25 +0600

    working on google oauth

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there', data:{bye:'buddy'} })
+  res.send({ hi: 'there', data:{bye:'budy'} })
 })
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); 
