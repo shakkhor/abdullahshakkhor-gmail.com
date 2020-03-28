2d1732b4d0c4963c54e3fb160a463d3a38eb3699
shakkhor <abdullahshakkhor@gmail.com>
2020-03-28 23:51:54 +0600

    authentication done

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+require('./models/User')
 require('./services/passport')
 
-const app = express();
+mongoose.connect(keys.mongoURI)
+const app = express()
 
-require('./routes/authRoutes')(app);
+app.use(
+  cookieSession({
+    maxAge: 10 * 24 * 60 * 60 * 100,
+    keys: [keys.cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,36 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('../config/keys')
+const mongoose = require('mongoose')
+const User = require('../models/User')
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user)
+  })
+})
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecrect,
-        callbackURL: '/auth/google/callback'
-      },
-      (accessToken,refreshToken,profile,done) => {
-        console.log(profile)
-      }
-    )
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecrect,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(user => {
+        if (user == null) {
+          new User({ googleId: profile.id }).save().then(user => {
+            done(null, user)
+          })
+        } else {
+          done(null, user)
+        }
+      })
+    }
   )
-  
+)
